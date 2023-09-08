b1215502e5caac96447ef2aa8e654576f8dd35c0
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-08 12:14:37 -0700

    program that prints alohabets in lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints alohabet in lower case
+ *
+ * Return: 0 (success)
+*/
+
+int main (void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
