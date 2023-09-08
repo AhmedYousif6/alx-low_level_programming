b002bae8b87c27aba4d8e54ca9f924bcb3fcdc60
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-08 12:53:48 -0700

    prints alpha but no e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: prints alphabet but no e and q
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	if (c == 'e' || c == 'q')
+		c++;
+		putchar('c');
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
