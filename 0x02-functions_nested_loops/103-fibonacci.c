587188ca4e5383252ad136ea5acf410e6e1bf91e
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-13 15:09:55 -0700

    task 14

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: print the sum of even values in fibonocci sequinse
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int prev1 = 1;
+	int prev2 = 2;
+	int sum = 0;
+
+	while (prev2 <= 4000000)
+	{
+		if (prev2 % 2 == 0)
+		sum += prev2;
+		printf("%d", sum);
+	int current = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = current;
+	}
+	printf("\n");
+	return (0);
+}
