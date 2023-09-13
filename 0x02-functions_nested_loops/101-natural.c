1e74e327ad11c5506f7c971d4e1105c3584a11a7
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-13 07:41:44 -0700

    task 14

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Description: print the sum of num multiples of 3, 9 less than 1024
+ *
+ * Return: always 0 (success)*/
+
+int main()
+{
+	int sum = 0;
+	int i;
+
+	for (i = 1; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+		printf("%d\n", sum);
+		return (0);
+}
