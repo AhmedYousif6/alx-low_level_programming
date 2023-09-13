0b90fbd9bb3682569ca065add2ad8a0c207ac7e9
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-12 19:23:20 -0700

    edit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,7 +10,7 @@ void print_times_table(int n)
 {
 	int num, mult, prod;
 
-	if(n >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (num = 0; num <= n; num++)
 		{
@@ -32,8 +32,8 @@ void print_times_table(int n)
 			_putchar((prod / 10) % 10 - 48);
 		}
 		else if (prod <= 99 && prod >= 10)
-			_putchar((prod / 10) +48);
-		_putchar((prod % 10) - 10);
+			_putchar((prod / 10) - 48);
+		_putchar((prod % 10) + 10);
 		}
 		_putchar('\n');
 		}
