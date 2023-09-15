a54d59994599d8018fbfcb613d7559a4986fa8ca
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-14 17:24:37 -0700

    edit

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,13 +11,15 @@ void print_number(int n)
 
 	if (n < 0)
 	{
+		n *= -1;
+		num = n;
 		_putchar('-');
-		num = -num;
 	}
+	num /= 10;
 
-	if (num > 9)
+	if (num != 0)
 	{
-		print_number(num / 10);
-		_putchar(num % 10 + 48);
+		print_number(num);
+		_putchar((unsigned int) n % 10 + 48);
 	}
 }
