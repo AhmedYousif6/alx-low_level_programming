1258904e30424f65475d4fe3d5079236a8a2b71b
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-21 14:34:07 -0700

    task 5 and edit main file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - change lowercase to uppercase
+ * @str: takes string
+ * Return: str
+*/
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
+	}
+	return (str);
+}
