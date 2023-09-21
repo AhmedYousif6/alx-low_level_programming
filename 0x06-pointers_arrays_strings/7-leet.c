d4a2ff234e8c910ab3d0c350d4f4338d7e4498ad
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-21 15:49:44 -0700

    task 7 and edit main file

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @ch: takes string
+ * Return: pointer of string
+*/
+
+char *leet(char *ch)
+{
+	char *ptr = ch;
+	char letter[] = {'A', 'E', 'O', 'T', 'L'};
+	int value[] = {4, 3, 0, 7, 1};
+	unsigned int i;
+
+	while (*ch)
+	{
+		for (i = 0; i < sizeof(letter) / sizeof(char); i++)
+		{
+			if (*ch == letter[i] || *ch == letter[i] + 32)
+			{
+			*ch = 48 + value[i];
+			}
+		}
+	ch++;
+	}
+	return (ptr);
+}
