3dd16f3374e4e01dbdaae48134e4d54fdee22b64
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-25 13:59:00 -0700

    edit task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,9 +13,12 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] == s[i]; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
+			if (accept[j] == s[i])
+			{
 			return (s + i);
+			}
 		}
 	}
 	return ('\0');
