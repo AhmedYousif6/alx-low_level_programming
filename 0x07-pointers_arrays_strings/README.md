0940b5cd83c47f2e401152e9b47aea3edcbc5e14
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-25 05:41:57 -0700

    add files for this project

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+project even more pointers arrays and strings
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ _putchar - write the character c to stdout
+ 
+Dc: the character that print
+
+Return: on success 1
+	on error -1 returned
+*/
+
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
