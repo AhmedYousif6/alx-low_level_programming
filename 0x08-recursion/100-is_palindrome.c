479da3c11ad17f90912bf4b84e3d9b3884d48559
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-09-28 16:14:38 -0700

    task 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+int length(char *s);
+int check(char *s, int start, int end, int m);
+
+/**
+ * length - find the length of a string
+ * @s: takes string
+ * Return: the length
+*/
+
+int length(char *s)
+{
+	int count = 0;
+
+	if (*s > '\0')
+	{
+		count += length(s + 1) + 1;
+	}
+}
+
+/**
+ * is_palindrome - find if the string is palindmore
+ * @s: takes string
+ * Return: integer
+*/
+
+int is_palindrome(char *s)
+{
+	int end = length(s);
+
+	return (check(s, 0, end - 1, end % 2));
+}
+
+/**
+ * check - check if the string is palindmore
+ * @s: takes string
+ * @start: the start of a string
+ * @end: the end of a string
+ * @m: modulos
+ * Return: integer
+*/
+
+int check(char *s, int start, int end, int m)
+{
+	if ((start == end && m != 1) || (start == end + 1 && m == 0))
+	{
+		return (1);
+	}
+	else if (s[start] != s[end])
+	{
+		return (0);
+	}
+	else
+	{
+		return (check(s, start + 1, end - 1, m));
+	}
+}
