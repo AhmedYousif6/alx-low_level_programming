eb55025e7d453738b58a2c85c93782de5ee35212
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-10-02 10:25:37 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print number of passed arguments
+ * @argc: number of passed argument
+ * @argv: passed arguments
+ * Return: 0 always (success)
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
