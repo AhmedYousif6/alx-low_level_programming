8346f18fbaaa694a5957fda7f40677da940c58d4
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-10-07 15:30:26 -0700

    edit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,6 +17,8 @@ int *array_range(int min, int max)
 		return (NULL);
 	len = max - min + 1;
 	ptr = malloc(sizeof(int) * len);
+	if (ptr == 0)
+		return (NULL);
 	for (i = 0; i < len; i++)
 		ptr[i] = min++;
 	return (ptr);
