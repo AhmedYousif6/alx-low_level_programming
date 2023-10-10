d0907027d2074a5ecc4479805653759430f6faf1
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-10-10 15:36:04 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure named dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: structure contain the name and age and owner
+ * of a dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
