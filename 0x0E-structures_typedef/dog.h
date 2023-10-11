84f2ea37ba0332c7827bef82b288e5c2ce316818
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2023-10-10 17:14:17 -0700

    edit task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,7 +9,6 @@
  * Description: structure contain the name and age and owner
  * of a dog
 */
-
 struct dog
 {
 	char *name;
@@ -17,12 +16,10 @@ struct dog
 	char *owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
