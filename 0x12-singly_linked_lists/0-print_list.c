29dfc337a023f7354fe82fdaf9b7c9deef3fcae6
AhmedYousif6 <ahmedmax0909090909@gamil.com>
2023-10-19 18:11:48 -0700

    modify files for task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,10 +1,30 @@
 #include "lists.h"
 
 /**
+ * _strlen - find the length of a string.
+ * @ss: takes string.
+ * Return: the length of the string.
+ */
+
+int _strlen(char *ss)
+{
+	int i = 0;
+
+	if (!ss)
+		return (0);
+	while (*ss)
+	{
+		i++;
+		ss++;
+	}
+	return (i);
+}
+
+/**
  * print_list - print a linked list.
  * @h: pointer to the first node.
  * Return: size of the linked list.
-*/
+ */
 
 size_t print_list(const list_t *h)
 {
@@ -12,9 +32,9 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		printf("[%d] &s\n", strlen(h->str), h->str ? h->str : "(nil)";
-				h = h->next;
-				count;
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
+		h = h->next;
+		count++;
 	}
 	return (count);
 }
