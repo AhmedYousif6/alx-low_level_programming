3fb168de47a5895c5d69927b6c61784e9165d2b0
AhmedYousif6 <ahmedmax0909090909@gamil.com>
2023-10-20 18:58:15 -0700

    edit task 101 file

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,10 +3,9 @@ section		.text
 	global	main
 
 main:
-	mov		edi, dat
-	mov		eax, 0
-	call	printf
+	mov	edi, dat
+	mov	eax, 0
+	call printf
 
-
-section		.data
-	dat db 'Hello, Holberton', 0xa, 0
+section         .data
+        dat db 'Hello, Holberton', 0xa, 0
