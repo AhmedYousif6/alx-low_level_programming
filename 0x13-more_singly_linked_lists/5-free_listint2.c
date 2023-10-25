b152956dbb3d0c7e85ea186ad64683c3f4cf284e
AhmedYousif6 <ahmedmax0909090909@gamil.com>
2023-10-24 17:26:31 -0700

    edit task 5 again

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,12 +9,12 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp = *head;
+	listint_t *tmp;
 	listint_t *ptr;
 
 	if (!head)
 		return;
-
+	tmp = *head;
 	while (tmp != NULL)
 	{
 		ptr = tmp;
